fd8eb2a40d3059446c6a58909324d019dfd5e306
Redwan islam <redwanislam2760@gmail.com>
2023-06-07 10:28:01 +0600

    complete get all users rout setup

--- index.js
+++ index.js
@@ -40,6 +40,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const userCollection = client.db("photographyDB").collection('users')
+
+/* -------------------------------------------------------------------------- */
+/*                                  GET ROUTE                                 */
+/* -------------------------------------------------------------------------- */
+
+
+    
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                                    POST ROUTE                                */
+    /* -------------------------------------------------------------------------- */
+        app.post("/users", async (req, res) => {
+          const user = req.body;
+          const query = { email: user.email };
+          const existingUser = await userCollection.findOne(query);
+          if (existingUser) {
+            return res.send({ message: "user already exists" });
+          }
+          const result = await userCollection.insertOne(user);
+
+          res.send(result);
+        });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
