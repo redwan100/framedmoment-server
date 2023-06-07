294ead1c034f6df7b345d68d0daabdf651c034c1
Redwan islam <redwanislam2760@gmail.com>
2023-06-07 08:32:36 +0600

    mongodb route setup complete

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 const app = express();
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 MIDDLEWARE                                 */
+/* -------------------------------------------------------------------------- */
+app.use(express.json())
+app.use(cors())
+
+/* -------------------------------------------------------------------------- */
+/*                                   ROUTES                                   */
+/* -------------------------------------------------------------------------- */
+app.get('/', (req, res) => {
+    res.send('Photography server is running')
+})
+
+
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yq2vgbi.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+/* -------------------------------------------------------------------------- */
+/*                                  LISTENER                                  */
+/* -------------------------------------------------------------------------- */
+
+app.listen(port, ()=>{
+    console.log(`Photography is listening on port ${port}`);
+})
